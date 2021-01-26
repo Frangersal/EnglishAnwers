b25687bdc958160fd4f8ca038c2abda007e91187
Frangersa <frangersal@outlook.com>
2021-01-25 18:53:58 -0600

    first commit

---
+++ public/js/index.js
@@ -0,0 +1,29 @@
+const english = new Vue({
+    el:"",
+    data: {
+
+        simplePast:[
+            "I played basketball with my sister.",
+            "You cooked the dinner tonight.",
+            "We began a project the last week ago.",
+            "She did not want to go to the theater.",
+            "I did not study today.",
+            "Did you want eat this sandwich?",
+            "Did she write all this book?"
+        ],
+
+    },
+    watch: {
+
+    },
+    filters: {
+
+    },
+    computed: {
+
+    },
+    methods: {
+        
+    },
+
+});
